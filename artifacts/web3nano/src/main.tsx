15e7fc1f6fc61b8dce0a671fdbad46a0208d5c26
abrotech001 <abrotexh@googlemail.com>
2026-04-18 10:22:27 +0100

    Update main.tsx

--- artifacts/web3nano/src/main.tsx
+++ artifacts/web3nano/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { I18nProvider } from "@/lib/i18n"; // 1. Import the provider
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  // 2. Wrap your App component
+  <I18nProvider>
+    <App />
+  </I18nProvider>
+);
